a42f7d78377c795d68ef78c26d1b55912c28f506
Crean <crean2@llnl.gov>
2018-04-12 15:07:00 -0700

    Web pack and stuff

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from '../node_modules/react'
+
+class App extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>Hello From React</div>
+        )
+    }
+}
+
+export default App
---
+++ frontend/webpack.config.js
@@ -0,0 +1,54 @@
+/* global __dirname */
+var path = require("path");
+
+module.exports = {
+    entry: ["babel-polyfill", "./src/index.js"],
+    devtool: 'source-map',
+    output: {
+        path: __dirname + "/dist",
+        filename: "Bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                'react',
+                                ['env', {
+                                    "targets": {
+                                        "uglify": true
+                                    }
+                                }]
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings 
+                }, {
+                    loader: "css-loader", // translates CSS into CommonJS 
+                }, {
+                    loader: "sass-loader", // compiles Sass to CSS 
+                    options: {
+                        includePaths: ["src"]
+                    }
+                }]
+            }
+        ]
+    }
+};
